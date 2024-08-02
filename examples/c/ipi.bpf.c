90fd3b6580e13c3cd3d4fcd393f45f665c44a55e
inwardvessel <5782523+inwardvessel@users.noreply.github.com>
2024-08-02 11:29:23 -0700

    ipi tracepoint

---
+++ examples/c/ipi.bpf.c
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: GPL-2.0 OR BSD-3-Clause
+/* Copyright (c) 2020 Facebook */
+#include "vmlinux.h"
+#include <bpf/bpf_helpers.h>
+
+char LICENSE[] SEC("license") = "Dual BSD/GPL";
+
+bool bpf_cpumask_set_cpu(u32 cpu, struct cpumask *cpumask) __ksym;
+bool bpf_cpumask_test_cpu(u32 cpu, const struct cpumask *cpumask) __ksym;
+
+SEC("tp/ipi/ipi_send_cpumask")
+int handle_tp(void *ctx)
+{
+	struct cpumask cpumask = {};
+	u32 cpu = bpf_get_smp_processor_id();
+
+	bpf_cpumask_set_cpu(cpu, &cpumask);
+
+	if (bpf_cpumask_test_cpu(cpu, &cpumask)) {
+		bpf_printk("cpu set\n");
+	} else {
+		bpf_printk("cpu NOT set\n");
+	}
+
+	return 0;
+}
