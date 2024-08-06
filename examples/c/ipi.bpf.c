2673cc42627cf4efaca51c001eebde4e0a47e6ea
inwardvessel <5782523+inwardvessel@users.noreply.github.com>
2024-08-06 13:33:31 -0700

    csd tracepoints and interrupt

--- examples/c/ipi.bpf.c
+++ examples/c/ipi.bpf.c
@@ -2,28 +2,65 @@
 /* Copyright (c) 2020 Facebook */
 #include "vmlinux.h"
 #include <bpf/bpf_helpers.h>
-#include <bpf/libbpf.h>
 
 char LICENSE[] SEC("license") = "Dual BSD/GPL";
 
-bool bpf_cpumask_set_cpu(u32 cpu, struct bpf_cpumask *cpumask) __ksym;
-bool bpf_cpumask_test_cpu(u32 cpu, const struct cpumask *cpumask) __ksym;
+extern bool bpf_cpumask_test_cpu(u32 cpu, const struct cpumask *cpumask) __ksym;
 
-SEC("tp/ipi/ipi_send_cpumask")
-int handle_tp(void *ctx)
+static const unsigned int nr_cpus = 16;
+
+SEC("tracepoint/csd/csd_queue_cpu")
+int handle_csd_queue(struct trace_event_raw_csd_queue_cpu *ctx)
 {
-	struct cpumask orig = {};
-	struct bpf_cpumask *cpumask = bpf_cpumask_create();
-	u32 cpu = bpf_get_smp_processor_id();
+	unsigned int cpu = (unsigned int)ctx->cpu;
+	void *func = (void *)ctx->func;
 
-	bpf_cpumask_copy(&orig, cpumask);
-	bpf_cpumask_set_cpu(cpu, &cpumask);
+	return 0;
+}
 
-	if (bpf_cpumask_test_cpu(cpu, &cpumask)) {
-		bpf_printk("cpu set\n");
-	} else {
-		bpf_printk("cpu NOT set\n");
+SEC("tp/csd/csd_function_entry")
+int handle_csd_function_entry(struct trace_event_raw_csd_function *ctx)
+{
+	return 0;
+}
+
+SEC("tp/csd/csd_function_exit")
+int handle_csd_function_exit(struct trace_event_raw_csd_function *ctx)
+{
+	return 0;
+}
+
+SEC("tp/ipi/ipi_send_cpu")
+int handle_ipi_send_cpu(struct trace_event_raw_ipi_send_cpu *ctx)
+{
+	return 0;
+}
+
+SEC("tp_btf/ipi_send_cpumask")
+int handle_tp(struct bpf_raw_tracepoint_args *ctx)
+{
+	struct cpumask *cpumask = (struct cpumask *)ctx->args[0];
+	u32 i, cpu = bpf_get_smp_processor_id();
+
+	for (i = 0; i < nr_cpus; ++i) {
+		if (bpf_cpumask_test_cpu(i, cpumask)) {
+			return 0;
+		}
 	}
 
+	return 0;
+}
+
+SEC("fentry/generic_smp_call_function_single_interrupt")
+int handle_call_function_single_entry(void)
+{
+	bpf_printk("call function single\n");
+	return 0;
+}
+
+SEC("fexit/generic_smp_call_function_single_interrupt")
+int handle_call_function_single_exit(void)
+{
+	bpf_printk("call function single exit\n");
 	return 0;
 }
